79d606cf5f67f6cb788155f5fdf002b423c20722
ComandanteTK <descontraindolive@gmail.com>
2023-08-04 23:18:06 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+document.getElementById("card").style.setProperty("display", "none", "important")
+
+function Procurar(){
+    document
+      .getElementById("card")
+      .style.setProperty("display", "block", "important");
+
+    let país = document.getElementById("pais").value
+    let finalURL = `https://restcountries.com/v3.1/name/${país}?fullText=true`
+    console.log(finalURL)
+    fetch(finalURL)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data[0])
+        console.log(data[0].population)
+        console.log(data[0].capital[0])
+        console.log(data[0].continents[0])
+
+        let bandeira = document.getElementById("bandeira")
+        let nome = document.getElementById("nome")
+        let capital = document.getElementById("capital")
+        let cont = document.getElementById("continente")
+        let pop = document.getElementById("populacao")
+
+        nome.innerHTML = país
+        capital.innerHTML = data[0].capital
+        cont.innerHTML = data[0].continents
+        pop.innerHTML = data[0].population
+        bandeira.src = data[0].flags.svg
+
+    })
+}
